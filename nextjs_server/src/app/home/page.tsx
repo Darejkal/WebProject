6d9bd7b07d1d815ff950b74d40d2ea721ee229ba
NguyenVanAnit <annit1910@gmail.com>
2024-04-29 15:41:33 +0700

    Creat module teacher

--- nextjs_server/src/app/home/page.tsx
+++ nextjs_server/src/app/home/page.tsx
@@ -1,6 +1,23 @@
+'use client'
+
+import { sign } from "crypto";
+import { useRouter } from "next/navigation";
+import { Button } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 export default function IndexPage(){
-    return (<div >
-        <a href="/user/login">Login</a><br />
-        <a href="/user/signup">Signup</a>
+    const router = useRouter()
+
+    const loginbtn = () => {
+        router.push("/user/login")
+    }
+
+    const signupbtn = () => {
+        router.push("/user/signup")
+    }
+
+    return (<div className="d-flex flex-column h-100 w-100 justify-content-center align-items-center">
+        <Button variant="success" onClick={() => loginbtn()}>Login</Button><br /><br />
+        <Button variant="primary" onClick={() => signupbtn()}>Signup</Button>
     </div>);
 }
--- nextjs_server/src/app/page.tsx
+++ nextjs_server/src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 export default function Home() {
   return (
       <div className="d-flex flex-column h-100 w-100 justify-content-center align-items-center">
+        <h1>Home Web</h1>
         <a href={`http://${process.env.APIHOST}`} className="">
           Goto Golang Chat
         </a>
