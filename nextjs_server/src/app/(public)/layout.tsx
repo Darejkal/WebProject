d934eb172d90666a6496020f66ba07b1c58b1f21
NguyenVanAnit <annit1910@gmail.com>
2024-06-05 18:06:09 +0700

    Reinstall the login and signup page

--- nextjs_server/src/app/(public)/layout.tsx
+++ nextjs_server/src/app/(public)/layout.tsx
@@ -7,9 +7,13 @@ export default function AuthenticationLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<div className="h-100 w-100 ">
-			<AppHeader redirect={false} />
-			{children}
+		<div className="h-100 w-100 " style={{margin: 0, padding: 0, boxSizing: 'border-box', display: 'grid', gridTemplateRows: 'auto 1fr', height: '100vh',}}>
+			<div style={{ gridRow: 1 }}>
+				<AppHeader redirect={false}/>
+			</div>
+			<div style={{ gridRow: 2, overflowY: 'auto', paddingBottom: '10%'}}>
+				{children}
+			</div>
 		</div>
 	);
 }
